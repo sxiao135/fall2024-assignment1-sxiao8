6d4744df538ad8d58a5af9018b10dc389b0603ba
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-09-23 18:00:46 -0500

    add project to directory

---
+++ fall2024-assignment1-sxiao8/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function addSoup() {
+    //console.log('button click function reached');
+    const img = document.createElement("img");
+    img.src = "../soupcan.jpeg";
+    img.height = 308.5;
+    img.width = 296.9;
+    img.alt = "soup can";
+    img.style = "border-radius: 0; height: 308.5px;";
+    //console.log(img.height, img.width);
+
+    img.style.position = 'absolute';
+    img.style.top = document.body.clientHeight * Math.random() + 'px';
+    img.style.left = document.body.clientWidth * Math.random() + 'px';
+
+    document.body.appendChild(img);
+}
